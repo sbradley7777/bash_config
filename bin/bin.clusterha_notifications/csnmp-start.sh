e2c0a18ba82321725cdfad0bcf9d0a9d5da42a50
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2025-12-13 14:41:35 -0500

    Move and delete files.
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

--- bin/bin.clusterha_notifications/csnmp-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-service messagebus restart
-service snmpd start
-service foghorn start
-service corosync-notifyd start
--- bin/bin.clusterha_notifications/csnmp-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-service corosync-notifyd stop
-service foghorn stop
-service snmpd stop
-service messagebus stop
