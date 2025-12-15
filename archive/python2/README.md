9dc96d1e051935569542a3174c98e1ecd3d38e5a
Shane Bradley <68629+sbradley7777@users.noreply.github.com>
2025-12-15 09:21:09 -0500

    Remove archive directory.
    Signed-off-by: Shane Bradley <68629+sbradley7777@users.noreply.github.com>

--- archive/python2/README.md
+++
@@ -1,2 +0,0 @@
-##Introduction
-This directory contains python snippets. Some of the snippets may not run correctly, becuase they are used only for testing python.
--- archive/python2/inspection/inspectobject.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-# http://www.diveintopython.net/power_of_introspection/index.html
-myObject = dict()
-methodList = [method for method in dir(myObject) if callable(getattr(myObject, method))]
-for method in methodList:
-    print method
